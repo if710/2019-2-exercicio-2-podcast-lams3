88e88f5e26a3c0f82c0c4a280ecb0b92f1b225cb
Luca Ananias <lams3@cin.ufpe.br>
2019-09-17 16:56:56 -0300

    Step 2 done

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,34 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private val xmlLink = "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
+
+    private var itemFeeds: ArrayList<ItemFeed> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Carregamento assincrono do XML
+        doAsync {
+            try {
+                // Download do XML
+                val xmlText = URL(xmlLink).readText()
+
+                // Parse do XML
+                val xmlItems = Parser.parse(xmlText)
+
+                itemFeeds.addAll(xmlItems)
+
+            } catch (exception : Exception) {
+                Log.d("DEBUG", exception.toString())
+            }
+        }
     }
 }
