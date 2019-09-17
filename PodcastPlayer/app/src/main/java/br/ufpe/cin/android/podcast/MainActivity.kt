a0cfff002f67adc0794cd8df23ce63286d20df96
Luca Ananias <lams3@cin.ufpe.br>
2019-09-17 19:05:31 -0300

    Step 3 done

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -3,18 +3,28 @@ package br.ufpe.cin.android.podcast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import java.io.IOException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private val xmlLink = "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
 
-    private var itemFeeds: ArrayList<ItemFeed> = arrayListOf()
+    private var itemFeeds: MutableList<ItemFeed> = mutableListOf()
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Atribuindo o Adapter implementado, assim como um LinearLayoutManager ao RecyclerView
+        recyclerView = layoutRecyclerView
+        recyclerView.adapter = ItemFeedAdapter(itemFeeds, this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         // Carregamento assincrono do XML
         doAsync {
@@ -27,7 +37,11 @@ class MainActivity : AppCompatActivity() {
 
                 itemFeeds.addAll(xmlItems)
 
-            } catch (exception : Exception) {
+                // Atualiza a RecyclerView na thread principal
+                runOnUiThread {
+                    (recyclerView.adapter as ItemFeedAdapter).notifyDataSetChanged()
+                }
+            } catch (exception: Exception) {
                 Log.d("DEBUG", exception.toString())
             }
         }
